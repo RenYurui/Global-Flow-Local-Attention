13e98ba39363324f5f28e202d38ec6895df168af
yurui <renyuruichn@gmail.com>
2020-03-07 17:03:03 +0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+cd ./model/networks/block_extractor
+python setup.py install --user
+
+cd ..
+cd block_extractor
+python setup.py install --user
+
+cd ..
+cd resample2d_package
+python setup.py install --user
