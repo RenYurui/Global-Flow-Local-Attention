8d8a0b4a4c61f7f947ccb9020870de75076e7099
Andrew Jong <andrewjong87@gmail.com>
2020-05-23 18:20:01 -0700

    Add clean to setup script
    In case user messes up the first install, adding the `clean --all` in the setup command will ensure a clean build each time.

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 cd ./model/networks/block_extractor
-python setup.py install --user
+python setup.py clean --all install --user
 
 cd ..
 cd local_attn_reshape
-python setup.py install --user
+python setup.py clean --all install --user
 
 cd ..
 cd resample2d_package
-python setup.py install --user
+python setup.py clean --all install --user
