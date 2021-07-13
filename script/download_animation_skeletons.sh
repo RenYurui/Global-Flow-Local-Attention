08acd04596f34edc50aa02c58083090405ac1f56
Ren Yurui <renyuruichn@qq.com>
2021-07-13 17:43:59 +0800

    Update download_animation_skeletons.sh

--- script/download_animation_skeletons.sh
+++ script/download_animation_skeletons.sh
@@ -1,4 +1,4 @@
-wget -O dataset/animation_skeletons.rar https://1drv.ms/u/s!ArirMHnmz_frlBfWgrPlnCFBJYiw?e=LOiHEI
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=17Fx56eJF_4-ky9GC8srh4sMuDwI3W0zq' -O dataset/animation_skeletons.rar
 cd dataset
 unrar x animation_skeletons.rar
 cd danceFashion/train_256
