7d5f8df8f0e7fdd94608f19b850ce521ad605396
Aleksey Morozov <36787333+amrzv@users.noreply.github.com>
2022-09-03 12:13:19 +0300

    Fix problem with downloading from google servers

--- download.sh
+++ download.sh
@@ -1,4 +1,4 @@
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1GyerWR0X-JDUIRs-AtmK_2iR7-J0jyZf' -O result.rar
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1oy2-IU1t383wG_yKuGwqvdWedDvxXS8V' -O dataset.rar
+gdown 1GyerWR0X-JDUIRs-AtmK_2iR7-J0jyZf -O result.rar
+gdown 1oy2-IU1t383wG_yKuGwqvdWedDvxXS8V -O dataset.rar
 unrar x result.rar
 unrar x dataset.rar
